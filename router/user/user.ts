9df465e53a2fd9648fa018461bcc4f5c713e700e
minzunim <greenteapig6@gmail.com>
2024-09-22 11:35:58 +0900

    [Feat] 회원가입/로그인 API 추가

---
+++ router/user/user.ts
@@ -0,0 +1,64 @@
+import { Router, Request, Response } from "express";
+const bcrypt = require('bcrypt');
+import db from "../../src/database";
+
+const router = Router();
+
+// 회원가입
+router.post("/join", async (req: Request, res: Response) => {
+
+    const { id, password } = req.body;
+
+    try {
+
+        const findSql = `SELECT idx FROM user WHERE id LIKE '${id}'`;
+        const findRes = await db.query(findSql);
+
+        if (findRes[0]) return res.json({ code: 400, msg: `동일한 아이디가 존재합니다.`, data: null });
+
+        const salt = await bcrypt.genSalt(10);
+
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const joinSql = `INSERT INTO user
+                            SET id = '${id}',
+                                password = '${hashPassword}'`;
+
+        const joinRes = await db.query(joinSql);
+        if (!joinRes) return res.json({ code: 401, msg: `회원 가입 도중 에러 발생`, data: null });
+
+        res.json({ code: 200, msg: `회원가입 완료`, data: null });
+
+    } catch (err) {
+        console.log(err);
+        res.json({ code: 500, msg: `서버 오류`, data: null });
+    }
+});
+
+// 로그인
+router.post("/login", async (req: Request, res: Response) => {
+
+    const { id, password } = req.body;
+
+    const findPasswordSql = `SELECT password FROM user WHERE id = '${id}'`;
+
+    try {
+        const passwordRes = await db.query(findPasswordSql);
+
+        if (!passwordRes.length) return res.json({ code: 400, msg: `아이디 또는 비밀번호를 확인해주세요.`, data: null });
+
+        const hashPassword = passwordRes[0].password;
+
+        const isMatchPassword = await bcrypt.compare(password, hashPassword);
+
+        if (!isMatchPassword) return res.json({ code: 401, msg: `아이디 또는 비밀번호를 확인해주세요.`, data: null });
+
+        res.json({ code: 200, msg: `로그인 성공`, data: null });
+
+    } catch (err) {
+        console.log(err);
+        res.json({ code: 500, msg: `서버 오류`, data: null });
+    }
+});
+
+export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,18 @@
 import express, { Request, Response, NextFunction } from 'express';
-const bcrypt = require('bcrypt');
+
+import user from "../router/user/user";
 
 const app = express();
+app.use(express.json());
 
-
-app.get("/welcome", (req: Request, res: Response, next: NextFunction) => {
-    res.send("welcome!");
+app.get("/welcome", (req: Request, res: Response) => {
+  res.send("welcome!");
 });
 
-app.post("/join", async (req: Request, res: Response) => {
-
-    const { id, password } = req.body;
-
-    try {
-        const salt = await bcrypt.gensalt(10);
-        const hashPassword = await bcrypt.hash(password, salt);
-
-        let userData = [];
-
-        // # TODO: DB 연결 필요
-        userData.push({ id, hashPassword });
-
-        res.json({ code: 200, msg: `회원가입 완료`, data: null });
-
-    } catch (err) {
-        res.json({ code: 500, msg: `서버 오류`, data: null });
-    }
-});
+app.use("/user", user);
 
 app.listen('1234', () => {
-    console.log(`
+  console.log(`
   ################################################
   🛡️  Server listening on port: 1234🛡️
   ################################################
